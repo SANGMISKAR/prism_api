26446c4b502b7af768d70a8a370782a90db1c979
Sanghmiskar Sanket Sahadeo Sujata <99116111+SANGMISKAR@users.noreply.github.com>
2025-03-01 13:13:31 +0530

    Update colorize.py

--- colorize.py
+++ colorize.py
@@ -1,7 +1,7 @@
 import requests
 
 # API endpoint
-API_URL = "https://magic-s7zz.onrender.com/upload"
+API_URL = "https://magic-1-b01n.onrender.com/upload"
 
 # Path to the image file you want to upload
 image_path = "girl.jpg"  # Replace with the actual path to your image
